4d8eede80efb01fca21ffc2dab29708c844c6fc0
ansmirnov <mail@ansmirnov.ru>
2014-06-20 13:59:51 +0400

    add db settings

--- epg_manager/epg_manager/settings.py
+++ epg_manager/epg_manager/settings.py
@@ -1,5 +1,7 @@
 # Django settings for epg_manager project.
 
+import os
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -7,12 +9,14 @@ ADMINS = (
     # ('Your Name', 'your_email@example.com'),
 )
 
+BASE_DIR = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + '/..')
+
 MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': BASE_DIR+'/map.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
