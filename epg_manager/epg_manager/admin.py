8fbb2f46f25264901f31e51932e68af3a34d164c
ansmirnov <mail@ansmirnov.ru>
2014-06-23 11:59:37 +0400

    delete admin.py

--- epg_manager/epg_manager/admin.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'Andrey Smirnov'
-__email__ = 'mail@ansmirnov.ru'
-
-from django.contrib import admin
-from epg_core import models
-
-admin.site.register(models.Channel)
-admin.site.register(models.Programme)
