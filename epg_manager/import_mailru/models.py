213b18f599bd41e007ef3de8d4e559e64d052d6b
ansmirnov <mail@ansmirnov.ru>
2014-06-25 10:50:04 +0400

    fix timezones

--- epg_manager/import_mailru/models.py
+++ epg_manager/import_mailru/models.py
@@ -5,6 +5,7 @@ from time import sleep
 import urllib3
 import json
 from clean_html import clean
+import pytz
 
 class MailRuChannel(models.Model):
     core_channel = models.ForeignKey(core_models.Channel)
@@ -12,6 +13,7 @@ class MailRuChannel(models.Model):
 
     def update(self):
         self.core_channel.clear_programmes()
+        greenwich = pytz.timezone('Greenwich')
         http = urllib3.PoolManager()
         r = http.request('GET', 'http://tv.mail.ru/ext/admtv/?sch.main=1&sch.channel=%d' % self.mailru_id)
         days = json.loads(r.data)['state']['days']
@@ -24,15 +26,11 @@ class MailRuChannel(models.Model):
             for programme in data['channel_type'].values()[0][0]['schedule']:
                 rdescr = http.request('GET', 'http://tv.mail.ru/ext/admtv/?sch.tv_event_id=%s&sch.channel_region_id=147' % (programme['id']))
                 descr = clean(json.loads(rdescr.data)['tv_event']['descr'])
-                start = datetime.strptime(programme['start'], '%Y-%m-%d %H:%M:%S')
-                start.timezone('Europe/Moscow')
-                stop = datetime.strptime(programme['stop'], '%Y-%m-%d %H:%M:%S')
-                stop.timezone('Europe/Moscow')
                 core_models.Programme(
                     channel=self.core_channel,
                     name=programme['name'],
-                    start=start,
-                    stop=stop,
+                    start=greenwich.localize(datetime.strptime(programme['start'], '%Y-%m-%d %H:%M:%S')),
+                    stop=greenwich.localize(datetime.strptime(programme['stop'], '%Y-%m-%d %H:%M:%S')),
                     description=descr,
                 ).save()
                 sleep(2)
